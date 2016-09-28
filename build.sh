7b8141a9ea375364d4924f4e6b92a79810b1fa58
sadoneli <sadoneli@gmail.com>
2016-09-28 17:23:14 +0800

    reduce some files

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-rules_date="2016-9-27 21:15"
-video_date="2016-9-27 20:50"
-
 rules_md5=`md5sum koolproxy.txt|tr " " "\n"|sed -n 1p`
 video_md5=`md5sum 1.dat|tr " " "\n"|sed -n 1p`
-# rule_date=`stat koolproxy.txt|grep Modify|awk '{print $2,$3}'|cut -d"." -f1`
+rule_date=`stat koolproxy.txt|grep Modify|awk '{print $2,$3}'|cut -d"." -f1`
 # rules_date=`cat koolproxy.txt | awk 'NR==1{print}' | awk '{print $3,$4}'`
-# video_date=`stat 1.dat|grep Modify|awk '{print $2,$3}'|cut -d"." -f1`
+video_date=`stat 1.dat|grep Modify|awk '{print $2,$3}'|cut -d"." -f1`
 
 cat > ./version <<EOF
 $rules_md5
--- config.json.js
+++
@@ -1,6 +0,0 @@
-{
-"rules_md5":"29e4eafa1a6c24b3eb8f3d35cd886d26",
-"rules_date":"2016-9-27 21:15"
-"video_md5":"4c526b9b8319740db99f6b0ea860a278",
-"video_date":"2016-9-27 20:50"
-}
